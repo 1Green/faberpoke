e734f61f763848ff20c11e5d66fe1f14f6ae110e
Ruben De Andrade <ruben.deandrade@fabernovel.com>
2021-03-02 09:54:12 +0100

    Remove void before import; Add catch
    Signed-off-by: Ruben De Andrade <ruben.deandrade@fabernovel.com>

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -2,12 +2,14 @@ import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry != null && onPerfEntry instanceof Function) {
-    void import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
+    }).catch((error: Error) => {
+      console.log("error: ", error)
     });
   }
 };
