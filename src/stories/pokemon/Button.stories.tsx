8cd38bb0916ca9e9c61245d1d35b116748882599
William Goulois <william.goulois@fabernovel.com>
2021-03-04 11:25:29 +0100

    Button with background img storybook

--- src/stories/pokemon/Button.stories.tsx
+++ src/stories/pokemon/Button.stories.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-
 import { Button, ButtonProps } from './Button';
 
 export default {
-  title: 'Button Button',
+  title: 'Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
-    className: { control: 'text' },
     label: { control: 'text' },
-    onClick: { control: 'object' }
+    onClick: { control: 'object' },
+    imgUrl: { control: 'object' },
   },
 } as Meta;
 
@@ -21,7 +20,13 @@ export const Shiny = Template.bind({});
 Shiny.args = {
   label: 'Shiny',
 };
-export const Gender = Template.bind({});
-Gender.args = {
+export const Female = Template.bind({});
+Female.args = {
   label: 'Gender',
+  imgUrl: '/img/female.png'
 };
+
+// Assume image.png is located in the "public" directory.
+export const withAnImage = () => (
+  <img src="/img/female.png" alt="test" />
+);
