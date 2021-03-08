a0b9679496b3d2970ede1a5fd4bdfeb4067c3682
Ruben De Andrade <ruben.deandrade@fabernovel.com>
2021-03-08 18:27:22 +0100

    Add Transition group; creates transitions
    Signed-off-by: Ruben De Andrade <ruben.deandrade@fabernovel.com>

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,42 +1,77 @@
-import React, { FunctionComponent, useRef } from 'react'
-import './navigation.css';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { FunctionComponent, useEffect, useRef, useState } from 'react'
+import {
+    CSSTransition,
+    TransitionGroup,
+} from 'react-transition-group';
+// import fadeTransition from './fade.module.css';  
+import slideTransition from './slide.module.css';  
+import styles from './navigation.module.css';
 
 export type NavigationProps = {
-    children: React.ReactNodeArray;
+    children: JSX.Element[];
 }
+
+const duration = 300
+//     onEnter = (node: { style: { marginTop: string; marginBottom: string; }; offsetHeight: number; }) => {
+//         node.style.marginTop = `-${node.offsetHeight}px`;
+//         node.style.marginBottom = `0px`;
+//     },
+//     onEntering = (node: { style: { marginTop: string; marginBottom: string; }; }) => {
+//         node.style.marginTop = "";
+//         node.style.marginBottom = "";
+//     },
+//     onExit = (node: { style: { marginTop: string; marginBottom: string; }; }) => {
+//         node.style.marginTop = "";
+//         node.style.marginBottom = "";
+//     },
+//     onExiting = (node: { style: { marginTop: string; marginBottom: string; }; offsetHeight: number; }) => {
+//         node.style.marginTop = `-${node.offsetHeight}px`;
+//         node.style.marginBottom = `0px`;
+//     };
 
 const Navigation: FunctionComponent<NavigationProps> = ({
     children
 }) => {
-    const refs = useRef<HTMLDivElement>(null);
-    const navigatorRef = useRef<HTMLDivElement>(null);
+    const sectionArray = children;
+    const [currentKey, setCurrentKey] = useState<string>(children[0] !== undefined ? children[0].key as string : '');
+    const [currentSection, setCurrentSection] = useState<JSX.Element[] | undefined>();
 
-    const scrollTo = () => {
-        console.log("scroll to: ", refs.current?.offsetTop)
-        navigatorRef.current?.scrollTo({ behavior: 'smooth', top: refs.current?.offsetTop })
-    }
+    useEffect(() => {
+        const newCurrentSection = sectionArray.filter(elem => elem.key === currentKey);
+        console.log("new current section : ", newCurrentSection);
+        setCurrentSection(newCurrentSection)
+    }, [currentKey, sectionArray])
 
-    console.log("refs: ", refs)
+
     return (
         <div style={{ height: '100vh', overflow: 'hidden' }}>
             <div style={{ position: 'absolute', top: '50%', right: '0px'}}>
                 <div>
-                    <button onClick={scrollTo}>
-                        <p>Scroll to</p>
+                    <button onClick={() => setCurrentKey('pokedex')}>
+                        <p>Pokedex</p>
+                    </button>
+                    <button onClick={() => setCurrentKey('roaster')}>
+                        <p>Roaster</p>
                     </button>
                 </div>
             </div>
-            <div ref={navigatorRef} className='navigation-wrapper'>
-                {children.map((child, index) => {
-                    if (child === undefined || child === null) return null;
-                    console.log("index: ", index);
-                    return (
-                        <div ref={refs} className='navigation-item' key={index}>
+            <TransitionGroup
+                className={styles.navigationWrapper}
+                timeout={duration}
+                // onEnter={onEnter}
+                // onEntering={onEntering}
+                // onExit={onExit}
+                // onExiting={onExiting}
+            >
+                {currentSection?.map((child: React.ReactChild) => (
+                    <CSSTransition key={currentKey} timeout={500} classNames={slideTransition}>
+                        <div className={styles.navigationItem}>
                             {child}
                         </div>
-                    )
-                })}
-            </div>
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>
         </div>
     )
 }
