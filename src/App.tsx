def8157a6dc2cdf18b7e9f4f59873008b6c10ace
Ruben De Andrade <ruben.deandrade@fabernovel.com>
2021-03-11 11:14:16 +0100

    move fonts to public; created Roster components
    Signed-off-by: Ruben De Andrade <ruben.deandrade@fabernovel.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Pokemon } from './pokemon/Pokemon';
-import rawJson from './json/pikachu.json';
-
+import logo from './logo.svg';
 function App() {
   return (
-    <Pokemon key={rawJson.id} {...rawJson} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p style={{ fontFamily: "Raleway", fontWeight: 900 }}>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/stories/rosterCard/RosterCard.tsx
+++ src/stories/rosterCard/RosterCard.tsx
@@ -4,15 +4,27 @@ import cx from 'classnames';
 import Icon from '../Icon/Icon';
 
 export type RosterCardProps = {
-    color: 'red' | 'blue' | 'orange' | 'yellow' | 'green';
+    empty?: boolean;
+    className?: string;
+    color?: 'red' | 'blue' | 'orange' | 'yellow' | 'green';
+    onAdd?: () => void;
 }
 
 export const RosterCard: FunctionComponent<RosterCardProps> = ({
-    color
+    color,
+    empty,
+    onAdd,
+    className
 }) => {
-    const wrapperStyles = cx(styles.wrapper, styles[color]);
+    if (empty === true) return (
+        <div className={cx(styles.wrapper, styles.empty, className)} onClick={onAdd}>
+            <Icon name='add' className={styles.addIcon} />
+            <p className={styles.addTitle}>Add Pokemon</p>
+        </div>
+    )
+
     return (
-        <div className={wrapperStyles}>
+        <div className={cx(styles.wrapper, color !== undefined && styles[color], className)}>
             <Icon name='pokeball-outline' className={styles.bgIcon} />
             <div className={styles.imageWrapper}>
                 <img alt='pokemon' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png' className={styles.image} />
