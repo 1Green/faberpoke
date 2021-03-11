7a8b35b54cc00126c6bc8bc77460b416439f709c
Ruben De Andrade <ruben.deandrade@fabernovel.com>
2021-03-11 11:14:16 +0100

    move fonts to public; created Roster components
    Signed-off-by: Ruben De Andrade <ruben.deandrade@fabernovel.com>

--- src/stories/Icon/Icon.tsx
+++ src/stories/Icon/Icon.tsx
@@ -2,13 +2,15 @@ import React, { FunctionComponent, FC, SVGProps } from 'react'
 import cx from 'classnames';
 import { ReactComponent as SearchIcon } from './iconSvgs/search.svg';
 import { ReactComponent as PokeballOutlineIcon } from './iconSvgs/pokeball-outline.svg';
+import { ReactComponent as AddIcon } from './iconSvgs/add.svg';
 import './icon.css';
 
-type IconKeys = 'search' | 'pokeball-outline';
+type IconKeys = 'search' | 'pokeball-outline' | 'add';
 type Icons = { [key in IconKeys]: FC<SVGProps<SVGSVGElement>> } 
 const icons: Icons = {
-    "search": SearchIcon,
-    "pokeball-outline": PokeballOutlineIcon,
+    'search': SearchIcon,
+    'add': AddIcon,
+    'pokeball-outline': PokeballOutlineIcon,
 }
 
 export interface IconProps {
