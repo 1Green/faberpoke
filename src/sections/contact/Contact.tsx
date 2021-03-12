d24fe71a271bb6c1ffd00ccc9b1c3eb56b8c245c
William Goulois <37271970+williamgoulois@users.noreply.github.com>
2021-03-12 17:48:27 +0100

    about page with storybook (#11)
    * about page with storybook
    
    * patch component with default props and image size

---
+++ src/sections/contact/Contact.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styles from './contact.module.css'
+import { PersonalInfo, PersonalInfoProps } from './PersonalInfo'
+
+export type ContactProps = {
+    contactArray?: PersonalInfoProps[];
+    imageUrl?: string;
+    alt?: string;
+}
+
+export const contactDetails: PersonalInfoProps[] =
+    [
+        {
+            imageUrl: `img/ruben.png`,
+            name: 'Ruben de Andrade',
+            role: 'Junior Software Engineer',
+            email: 'ruben.deandrade@fabernovel.com',
+        },
+        {
+            imageUrl: `img/william.png`,
+            name: 'William Goulois',
+            role: 'Junior Software Engineer',
+            email: 'william.goulois@fabernovel.com',
+        },
+    ]
+
+export const squirtleImage = 'img/squirtle.png'
+export const squirtleAlt = "squirtle"
+
+export function Contact({ contactArray = contactDetails, imageUrl = squirtleImage, alt = squirtleAlt }: ContactProps) {
+    return (
+        <div className={styles.contactContainer}>
+            <div className={styles.personalInfoContainer}>
+                {
+                    contactArray.map(((contact) => {
+                        return <PersonalInfo {...contact} />
+                    }))
+                }
+            </div>
+            <img className={styles.image} src={imageUrl} alt={alt} />
+        </div>
+    )
+}
