17730c56c74a44b6f8f5a67572d6e8fcfb989208
Ruben De Andrade <ruben.deandrade@fabernovel.com>
2021-03-12 12:03:35 +0100

    Clean svgs; move navigation/ to components/
    Signed-off-by: Ruben De Andrade <ruben.deandrade@fabernovel.com>

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,39 +1,48 @@
-import React, { FunctionComponent, FC, SVGProps } from 'react'
-import cx from 'classnames';
-import { ReactComponent as SearchIcon } from './iconSvgs/search.svg';
-import { ReactComponent as CloseIcon } from './iconSvgs/close.svg';
-import './icon.css';
+import React, { FunctionComponent, FC, SVGProps } from "react";
+import cx from "classnames";
+import { ReactComponent as SearchIcon } from "./iconSvgs/search.svg";
+import { ReactComponent as CloseIcon } from "./iconSvgs/close.svg";
+import { ReactComponent as PokeBallIcon } from "./iconSvgs/poke-ball.svg";
+import { ReactComponent as SuperBallIcon } from "./iconSvgs/super-ball.svg";
+import { ReactComponent as MegaBallIcon } from "./iconSvgs/mega-ball.svg";
+import { ReactComponent as UltraBallIcon } from "./iconSvgs/ultra-ball.svg";
+import "./icon.css";
 
-type IconKeys = 'search' | 'close';
-type Icons = { [key in IconKeys]: FC<SVGProps<SVGSVGElement>> } 
+export type IconKeys =
+  | "search"
+  | "close"
+  | "pokeball"
+  | "superball"
+  | "megaball"
+  | "ultraball";
+type Icons = { [key in IconKeys]: FC<SVGProps<SVGSVGElement>> };
 const icons: Icons = {
-    'search': SearchIcon,
-    'close': CloseIcon
-}
+  search: SearchIcon,
+  close: CloseIcon,
+  pokeball: PokeBallIcon,
+  superball: SuperBallIcon,
+  megaball: MegaBallIcon,
+  ultraball: UltraBallIcon,
+};
 
 export interface IconProps {
-    name: IconKeys;
-    color?: string;
-    size?: 'small' | 'medium' | 'large';
-    style?: React.CSSProperties;
-    className?: string;
+  name: IconKeys;
+  color?: string;
+  size?: "small" | "medium" | "large";
+  style?: React.CSSProperties;
+  className?: string;
 }
 
 export const Icon: FunctionComponent<IconProps> = ({
-    name,
-    color,
-    className,
-    size = 'medium',
+  name,
+  color,
+  className,
+  size = "medium",
 }) => {
-    const CustomIcon = icons[name];
-    const iconClasses = cx(`icon--${size}`, className);
+  const CustomIcon = icons[name];
+  const iconClasses = cx(`icon--${size}`, className);
 
-    return (
-        <CustomIcon
-            fill={color}
-            className={iconClasses}
-        />
-    )
-}
+  return <CustomIcon fill={color} className={iconClasses} />;
+};
 
 export default Icon;
